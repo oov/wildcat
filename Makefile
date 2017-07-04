65e01abe7f39f217c06bef3be77428f3184b3841
oov <ilovevipper@gmail.com>
2017-07-05 05:43:19 +0900

    add test program

---
+++ Makefile
@@ -0,0 +1,39 @@
+EMCC = emcc
+EMCONF = emconfigure
+EMMAKE = emmake
+
+OGG_INCLUDE=$(CURDIR)/ogg/include
+OGG_LIBS=$(CURDIR)/ogg/src/.libs
+OPUS_INCLUDE=$(CURDIR)/opus/include
+OPUS_LIBS=$(CURDIR)/opus/.libs
+OPUSFILE_INCLUDE=$(CURDIR)/opusfile/include
+OPUSFILE_LIBS=$(CURDIR)/opusfile/.libs
+SPEEXDSP_INCLUDE=$(CURDIR)/speexdsp/include/speex
+SPEEXDSP_SRC=$(CURDIR)/speexdsp/libspeexdsp
+
+FILES = $(SPEEXDSP_SRC)/resample.c src/main.c
+CFLAGS = -O3 -I$(OGG_INCLUDE) -I$(OPUS_INCLUDE) -I$(OPUSFILE_INCLUDE) -I$(SPEEXDSP_INCLUDE) -DOUTSIDE_SPEEX -DRANDOM_PREFIX=spx -DFLOATING_POINT -DEXPORT=
+LIBS = $(OGG_LIBS)/libogg.so $(OPUS_LIBS)/libopus.so $(OPUSFILE_LIBS)/libopusfile.so
+
+ogg:
+	cd ogg && \
+	./autogen.sh && \
+	$(EMCONF) ./configure && \
+	$(EMMAKE) make
+
+opus:
+	cd opus && \
+	./autogen.sh && \
+	$(EMCONF) ./configure --disable-intrinsics --disable-doc --disable-extra-programs && \
+	$(EMMAKE) make
+
+opusfile:
+	cd opusfile && \
+	./autogen.sh && \
+	DEPS_CFLAGS="-I$(OGG_INCLUDE) -I$(OPUS_INCLUDE)" \
+	DEPS_LIBS="-L$(OGG_LIBS) -L$(OPUS_LIBS)" \
+	$(EMCONF) ./configure --disable-http --disable-examples --disable-doc && \
+	$(EMMAKE) make
+
+main: $(FILES)
+	$(EMCC) $(CFLAGS) $(FILES) $(LIBS) -o wildcat.html --llvm-lto 1 -s AGGRESSIVE_VARIABLE_ELIMINATION=1 -s NO_FILESYSTEM=1 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS="['_wc_open', '_wc_close', '_wc_seek', '_wc_tell', '_wc_buffer', '_wc_channels', '_wc_read']"
---
+++ src/main.c
@@ -0,0 +1,184 @@
+#include <stdlib.h>
+#include <opusfile.h>
+#include <emscripten/emscripten.h>
+#include <speex_resampler.h>
+
+// *IMPORATNT*
+// This program does not consider changes in the number of channels in the middle of a stream.
+
+void *opus_fileimage = NULL;
+OggOpusFile *ctx = NULL;
+SpeexResamplerState *resampler = NULL;
+int channels = 0;
+ogg_int64_t current_position = 0;
+
+int opus_buffer_size = 0;
+float *opus_buffer = NULL;
+
+int dest_buffer_size = 0;
+float *dest_buffer = NULL;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void EMSCRIPTEN_KEEPALIVE wc_close() {
+  if (dest_buffer != NULL) {
+    if (dest_buffer != opus_buffer) {
+      free(dest_buffer);
+    }
+    dest_buffer_size = 0;
+    dest_buffer = NULL;
+  }
+  if (resampler != NULL) {
+    spx_resampler_destroy(resampler);
+    resampler = NULL;
+  }
+  if (opus_buffer != NULL) {
+    free(opus_buffer);
+    opus_buffer_size = 0;
+    opus_buffer = NULL;
+  }
+  if (ctx != NULL) {
+    op_free(ctx);
+    ctx = NULL;
+  }
+  channels = 0;
+  if (opus_fileimage != NULL) {
+    free(opus_fileimage);
+    opus_fileimage = NULL;
+  }
+}
+
+// ~-1 = error / 0~ = pcm total length
+int EMSCRIPTEN_KEEPALIVE wc_open(void *p, int len, int target_samplerate) {
+  if (ctx != NULL) {
+    return -1;
+  }
+
+  int err;
+  OggOpusFile *newctx = op_open_memory(p, (size_t)len, &err);
+  if (newctx == NULL) {
+    return -1;
+  }
+
+  wc_close();
+
+  ctx = newctx;
+  opus_fileimage = p;
+  current_position = 0;
+
+  channels = op_channel_count(ctx, -1);
+  if (channels == 0) {
+    return -2;
+  }
+
+  const int BUFFER_SIZE_MSEC = 120;
+  opus_buffer_size = sizeof(float)*((48000*BUFFER_SIZE_MSEC)/1000)*channels;
+  opus_buffer = (float*)malloc(opus_buffer_size);
+  if (opus_buffer == NULL) {
+    wc_close();
+    return -3;
+  }
+
+  // If the target samplerate is not 48kHz, initialize the resampler
+  if (target_samplerate != 48000) {
+    resampler = spx_resampler_init(channels, 48000, target_samplerate, 5, &err);
+    if (resampler == NULL) {
+      wc_close();
+      return -4;
+    }
+    spx_resampler_skip_zeros(resampler);
+
+    dest_buffer_size = sizeof(float)*((target_samplerate*(BUFFER_SIZE_MSEC+1))/1000)*channels;
+    dest_buffer = (float*)malloc(dest_buffer_size);
+    if (dest_buffer == NULL) {
+      wc_close();
+      return -5;
+    }
+  } else {
+    dest_buffer_size = opus_buffer_size;
+    dest_buffer = opus_buffer;
+  }
+
+  ogg_int64_t r = op_pcm_total(ctx, -1);
+  if (r < 0) {
+    wc_close();
+    return -6;
+  }
+  return (int)r;
+}
+
+// ~-1 = error / 0 = ok
+int EMSCRIPTEN_KEEPALIVE wc_seek(int sample) {
+  if (ctx == NULL) {
+    return -1;
+  }
+  ogg_int64_t pos = (ogg_int64_t)sample;
+  int r = op_pcm_seek(ctx, pos);
+  if (r == 0) {
+    current_position = pos;
+  }
+  return r;
+}
+
+// ~-1 = error / 0~ = sample pos
+int EMSCRIPTEN_KEEPALIVE wc_tell() {
+  if (ctx == NULL) {
+    return -1;
+  }
+  return op_pcm_tell(ctx);
+}
+
+float *EMSCRIPTEN_KEEPALIVE wc_buffer() {
+  if (ctx == NULL) {
+    return NULL;
+  }
+  return dest_buffer;
+}
+
+// 0 = error
+int EMSCRIPTEN_KEEPALIVE wc_channels() {
+  if (ctx == NULL) {
+    return 0;
+  }
+  return channels;
+}
+
+int EMSCRIPTEN_KEEPALIVE wc_read(int end_sample_pos) {
+  if (ctx == NULL) {
+    return -1;
+  }
+  if (current_position >= end_sample_pos) {
+    return 0;
+  }
+  int read_size = opus_buffer_size / channels;
+  if (end_sample_pos - current_position < read_size) {
+    read_size = end_sample_pos - current_position;
+  }
+  for (;;) {
+    int written = op_read_float(ctx, opus_buffer, read_size * channels, NULL);
+    if (written == OP_HOLE) {
+      continue;
+    }
+    if (written < 0) {
+      return written;
+    }
+    current_position += written;
+    if (resampler == NULL) {
+      return written;
+    }
+    spx_uint32_t in_len = (spx_uint32_t)written, out_len = (spx_uint32_t)dest_buffer_size / channels;
+    int r = speex_resampler_process_interleaved_float(resampler, opus_buffer, &in_len, dest_buffer, &out_len);
+    if (r != RESAMPLER_ERR_SUCCESS) {
+      return r;
+    }
+    if (written != (int)in_len) {
+      return -2;
+    }
+    return (int)out_len;
+  }
+}
+#ifdef __cplusplus
+}
+#endif
